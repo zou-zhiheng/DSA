042dd8ce08b94fa367dac2031d428a6d1cff4276
sttch <421786612@qq.com>
2023-07-25 01:16:47 +0800

    lru

--- main.go
+++ main.go
@@ -1,9 +1,69 @@
 package main
 
-import "DSA/Slice"
+import (
+	"container/list"
+	"fmt"
+)
+
+type LRUCache struct {
+	capacity int
+	cache    map[int]*list.Element
+	list     *list.List
+}
+
+type Pair struct {
+	key   int
+	value int
+}
+
+func NewLRUCache(capacity int) *LRUCache {
+	return &LRUCache{
+		capacity: capacity,
+		cache:    make(map[int]*list.Element),
+		list:     list.New(),
+	}
+}
+
+func (c *LRUCache) Get(key int) int {
+	if elem, ok := c.cache[key]; ok {
+		c.list.MoveToFront(elem)
+		return elem.Value.(Pair).value
+	}
+	return -1
+}
+
+func (c *LRUCache) Put(key int, value int) {
+	if elem, ok := c.cache[key]; ok {
+		c.list.MoveToFront(elem)
+		elem.Value = Pair{key: key, value: value}
+	} else {
+		if c.list.Len() >= c.capacity {
+			delete(c.cache, c.list.Back().Value.(Pair).key)
+			c.list.Remove(c.list.Back())
+		}
+		c.list.PushFront(Pair{key: key, value: value})
+		c.cache[key] = c.list.Front()
+	}
+}
 
 func main() {
-	//Goroutine.Demo()
-	//WorkPool.Demo()
-	Slice.Demo()
+	cache := NewLRUCache(2)
+	cache.Put(1, 1)
+	cache.Put(2, 2)
+	fmt.Println(cache.Get(1)) // returns 1
+	cache.Put(3, 3)           // evicts key 2
+	fmt.Println(cache.Get(2)) // returns -1 (not found)
+	cache.Put(4, 4)           // evicts key 1
+	fmt.Println(cache.Get(1)) // returns -1 (not found)
+	fmt.Println(cache.Get(3)) // returns 3
+	fmt.Println(cache.Get(4)) // returns 4
 }
+
+//
+//import "DSA/Slice"
+//
+//func main() {
+//	//Goroutine.Demo()
+//	//WorkPool.Demo()
+//	Slice.Demo()
+//}
